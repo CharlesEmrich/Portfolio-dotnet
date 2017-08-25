959a459097d7939db0221713d613636e35a00080
Charles Emrich <libertinistic@gmail.com>
2017-08-25 14:42:08 -0700

    Inch forward. Currently there seems to be an authentication problem

--- Portfolio/Models/Repository.cs
+++ Portfolio/Models/Repository.cs
@@ -6,23 +6,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Diagnostics;
 
 namespace Portfolio.Models
 {
     public class Repository
     {
+        public string name { get; set; }
+        public string html_url { get; set; }
+        public int forks { get; set; }
+        public string description { get; set; }
+
         public static List<Repository> GetRepos()
         {
             var client = new RestClient("https://api.github.com/?access_token=" + EnvironmentVariables.AuthToken);
-            var request = new RestRequest("user/CharlesEmrich/repos", Method.GET);
-            // client.Authenticator = new HttpBasicAuthenticator("{{Account SID}}", "{{Auth Token}}");
+
+            RestRequest request = new RestRequest("users/CharlesEmrich/repos", Method.GET);
+
+            request.AddHeader("Accept", "application/json");
             var response = new RestResponse();
             Task.Run(async () =>
             {
                 response = await GetResponseContentAsync(client, request) as RestResponse;
+                Debug.WriteLine(response.Content);
             }).Wait();
             JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
-            var repoList = JsonConvert.DeserializeObject<List<Repository>>(jsonResponse["messages"].ToString());
+            var repoList = JsonConvert.DeserializeObject<List<Repository>>(jsonResponse["items"].ToString());
             return repoList;
         }
         public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
